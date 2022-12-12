b4162d06a723673f652a27185e198c16854b6c53
mikeiannotti55 <79390433+mikeiannotti55@users.noreply.github.com>
2022-12-12 12:59:29 -0600

    first commit

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(
-  "mongodb+srv://user123:Password123Tech@cluster0.j7fql.mongodb.net/merntutorial?retryWrites=true&w=majority"
+  "mongodb+srv://test:password1234@cluster0.l6ljynb.mongodb.net/?retryWrites=true&w=majority"
 );
 
 app.get("/getUsers", (req, res) => {
